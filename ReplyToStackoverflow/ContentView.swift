501917909e6c728a9377c92532abc9d5a0daddab
Taeeun Kim <taeeun.kim@outlook.com>
2021-07-16 14:29:39 +0200

    Reply at "SwiftUI how to pass data to previous screen on dismiss"

--- ReplyToStackoverflow/ContentView.swift
+++ ReplyToStackoverflow/ContentView.swift
@@ -8,28 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            TabView {
-                Text("Saturday")
-                Text("Sunday")
-                Text("Monday")
-                Text("Tuesday")
-                Text("Wednesday")
-                Text("Thursday")
-                Text("Friday")
-            }
-            .tabViewStyle(PageTabViewStyle())
-            .frame(width: 300, height: 600, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        VStack {
+            
         }
-        .frame(width: 300, height: 600, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-        .background(Color.blue)
-        .onAppear(perform: {
-            UIScrollView.appearance().alwaysBounceVertical = false
-        })
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ ReplyToStackoverflow/SwiftUI how to pass data to previous screen on dismiss.swift
@@ -0,0 +1,60 @@
+//
+//  SwiftUI how to pass data to previous screen on dismiss.swift
+//  ReplyToStackoverflow
+//
+//  Created by Taeeun Kim on 16.07.21.
+//
+
+// https://stackoverflow.com/questions/68408668/swiftui-how-to-pass-data-to-previous-screen-on-dismiss/68408865#68408865
+
+import SwiftUI
+
+struct SwiftUI_how_to_pass_data_to_previous_screen_on_dismiss: View {
+    @State var showModel = false
+    @State var filter = Filter(fromDate: "", toDate: "")
+    
+    var body: some View {
+        VStack {
+            Text("\(filter.fromDate) and \(filter.toDate)")
+            Button(action: {
+                showModel.toggle()
+            }, label: {
+                Text("Show filters")
+            }).sheet(isPresented: $showModel, content: {
+                FilterView(filter: $filter)
+            })
+        }
+    }
+}
+
+struct FilterView: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    var onDismiss: ((_ model: Filter) -> Void)?
+    @Binding var filter: Filter
+
+    var body: some View {
+        
+        VStack {
+            Button(action: {
+                // Pass data from here to ContentView
+                filter = Filter(fromDate: "10/07/2021", toDate: "12/07/2021")
+                onDismiss?(filter)
+                presentationMode.wrappedValue.dismiss()
+            }, label: {
+                Text("Applay Filters")
+            }).padding()
+        }.padding()
+    }
+}
+
+struct Filter {
+    var fromDate: String
+    var toDate: String
+}
+
+struct SwiftUI_how_to_pass_data_to_previous_screen_on_dismiss_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUI_how_to_pass_data_to_previous_screen_on_dismiss()
+    }
+}
